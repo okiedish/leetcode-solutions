c5f91b5eda9a2eab4840f4c446abd46b57d7393f
okiedish <161029692+okiedish@users.noreply.github.com>
2026-09-14 00:54:42 +0530

    Add solution for Two Sum

---
+++ solutions/easy/Two Sum.c
@@ -0,0 +1,19 @@
+// Title: Two Sum
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/two-sum/
+
+
+                int* result = malloc(2 * sizeof(int));
+            if (nums[i] + nums[j] == target) {
+
+        for (int j = i + 1; j < numsSize; j++) {
+
+
+    for (int i = 0; i < numsSize; i++) {
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+ */
+ * Note: The returned array must be malloced, assume caller calls free().
+/**
+
+#include <stdlib.h>
