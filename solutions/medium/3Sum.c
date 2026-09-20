1e579a8261e2077ffab1a826d16a9999a51c2678
okiedish <161029692+okiedish@users.noreply.github.com>
2026-09-20 21:21:02 +0530

    Add solution for 3Sum

---
+++ solutions/medium/3Sum.c
@@ -0,0 +1,19 @@
+// Title: 3Sum
+            // Difficulty: Medium
+            // Language: C
+            // Link: https://leetcode.com/problems/3sum/
+
+#include <stdlib.h>
+
+int cmp(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
+    *returnSize = 0;
+    if (numsSize < 3) return NULL;
