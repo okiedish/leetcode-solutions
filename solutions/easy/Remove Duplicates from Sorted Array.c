28425d0158dec9916ae4d0a5be276371445f1cb8
okiedish <161029692+okiedish@users.noreply.github.com>
2026-09-23 21:33:47 +0530

    Add solution for Remove Duplicates from Sorted Array

---
+++ solutions/easy/Remove Duplicates from Sorted Array.c
@@ -0,0 +1,18 @@
+// Title: Remove Duplicates from Sorted Array
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+int removeDuplicates(int* nums, int numsSize) {    
+    if (numsSize == 0) return 0; 
+
+    int k = 1; 
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] != nums[i - 1]) { 
+            nums[k] = nums[i]; 
+            k++; 
+        }
+    }
+    return k; 
+}
+
